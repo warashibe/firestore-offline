a187d968505b57da36dcada871addf9be58446a1
Tomoya Nagasawa <ocrybit@gmail.com>
2020-11-26 23:00:21 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,351 @@
+const {
+  without,
+  concat,
+  difference,
+  uniq,
+  reduce,
+  range,
+  pluck,
+  descend,
+  ascend,
+  sortWith,
+  intersection,
+  includes,
+  is,
+  cond,
+  always,
+  propEq,
+  reverse,
+  when,
+  sortBy,
+  path,
+  filter,
+  slice,
+  map,
+  mapObjIndexed,
+  values,
+  length,
+  o,
+  compose,
+  append,
+  isNil,
+  mergeLeft,
+  clone,
+  isEmpty
+} = require("ramda")
+
+const shortid = require("shortid")
+
+let config = null
+
+let db = {}
+
+function build(paths) {
+  let _db = db
+  for (const p of paths) {
+    if (isNil(_db[p])) _db[p] = {}
+    _db = _db[p]
+  }
+  return _db
+}
+
+function ref(paths) {
+  let _db = db
+  for (const p of paths) {
+    if (isNil(_db[p])) return null
+    _db = _db[p]
+  }
+  return _db
+}
+
+function doc(paths) {
+  return name => ({
+    collection: collection(append(name)(paths)),
+    get: () =>
+      new Promise((res, rej) => {
+        const data = ref(append(name, paths))
+        res({ exists: data !== null, data: () => data || null, key: name })
+      }),
+    update: data => {
+      new Promise((res, rej) => {
+        let _doc = build(paths)
+        if (isNil(_doc[name])) {
+          rej("doc desn't exist")
+        } else {
+          _doc[name] = make(data, _doc[name])
+          res(name)
+        }
+      })
+    },
+    delete: data => {
+      new Promise((res, rej) => {
+        let _doc = build(paths)
+        if (isNil(_doc[name])) {
+          rej("doc desn't exist")
+        } else {
+          delete _doc[name]
+          res(name)
+        }
+      })
+    },
+    set: (data, opt = {}) =>
+      new Promise((res, rej) => {
+        let _doc = build(paths)
+        if (isNil(_doc[name])) {
+          _doc[name] = data
+          res(name)
+        } else {
+          if (opt.merge) {
+            _doc[name] = make(data, _doc[name])
+            res(name)
+          } else {
+            rej("exists")
+          }
+        }
+      })
+  })
+}
+
+function make(data, old = {}) {
+  for (const k in data) {
+    let v = data[k]
+    if (is(Object, v) && v._FieldValue) {
+      if (v.key === "increment") {
+        if (isNil(old[k])) old[k] = 0
+        old[k] += v.val
+      } else if (v.key === "delete") {
+        delete old[k]
+      } else if (v.key === "serverTimestamp") {
+        old[k] = Date.now()
+      } else if (v.key === "arrayUnion") {
+        if (isNil(old[k])) old[k] = []
+        old[k] = concat(old[k])(difference(uniq(v.val), old[k]))
+      } else if (v.key === "arrayRemove") {
+        if (isNil(old[k])) old[k] = []
+        old[k] = without(v.val, uniq(old[k]))
+      }
+    } else {
+      old[k] = data[k]
+    }
+  }
+  return old
+}
+
+function collection(paths, opt = { where: [], orderBy: [] }) {
+  return name => ({
+    doc: doc(append(name)(paths)),
+    add: data =>
+      new Promise(res => {
+        let _col = build(append(name)(paths))
+        const id = shortid.generate()
+        _col[id] = make(data)
+        res(id)
+      }),
+    startAt: (...args) =>
+      collection(paths, mergeLeft({ startAt: args }, opt))(name),
+    startAfter: (...args) =>
+      collection(paths, mergeLeft({ startAfter: args }, opt))(name),
+    endAt: (...args) =>
+      collection(paths, mergeLeft({ endAt: args }, opt))(name),
+    endBefore: (...args) =>
+      collection(paths, mergeLeft({ endBefore: args }, opt))(name),
+    where: (field, op, val) =>
+      collection(
+        paths,
+        mergeLeft({ where: append({ field, op, val })(opt.where) }, opt)
+      )(name),
+    orderBy: (field, dir = "asc") =>
+      collection(
+        paths,
+        mergeLeft({ orderBy: append({ field, dir })(opt.orderBy) }, opt)
+      )(name),
+    limit: n => collection(paths, mergeLeft({ limit: n }, opt))(name),
+    get: () =>
+      new Promise(res => {
+        let data = o(values, mapObjIndexed((v, k) => ({ key: k, data: v })))(
+          ref(append(name)(paths))
+        )
+        for (const w of opt.where) {
+          data = filter(v => {
+            if (isNil(v.data[w.field])) return false
+            if (w.op === "==") {
+              return v.data[w.field] === w.val
+            } else if (w.op === "!=") {
+              return v.data[w.field] !== w.val
+            } else if (w.op === ">") {
+              return v.data[w.field] > w.val
+            } else if (w.op === ">=") {
+              return v.data[w.field] >= w.val
+            } else if (w.op === "<") {
+              return v.data[w.field] < w.val
+            } else if (w.op === "<=") {
+              return v.data[w.field] <= w.val
+            } else if (w.op === "in") {
+              return includes(v.data[w.field])(w.val)
+            } else if (w.op === "not-in") {
+              return !includes(v.data[w.field])(w.val)
+            } else if (w.op === "array-contains") {
+              return (
+                is(Array, v.data[w.field]) && includes(w.val)(v.data[w.field])
+              )
+            } else if (w.op === "array-contains-any") {
+              return (
+                is(Array, v.data[w.field]) &&
+                intersection(w.val)(v.data[w.field]).length !== 0
+              )
+            } else {
+              return true
+            }
+          })(data)
+        }
+        if (opt.orderBy.length !== 0) {
+          data = compose(
+            sortWith(
+              map(
+                v =>
+                  v.dir === "asc"
+                    ? ascend(path(["data", v.field]))
+                    : descend(path(["data", v.field]))
+              )(opt.orderBy)
+            ),
+            filter(v => {
+              for (let v2 of pluck("field")(opt.orderBy)) {
+                if (isNil(v.data[v2])) return false
+              }
+              return true
+            })
+          )(data)
+        }
+        if (!isNil(opt.startAt)) {
+          const min = Math.min(opt.startAt.length, opt.orderBy.length)
+          if (min > 0) {
+            for (const i in range(0, min)) {
+              let ex = false
+              data = filter(v => {
+                if (ex === true) return true
+                if (typeof opt.startAt[i] === "object") {
+                  ex = v.key === opt.startAt[i].key
+                } else {
+                  ex =
+                    opt.orderBy[i].dir === "asc"
+                      ? v.data[opt.orderBy[i].field] >= opt.startAt[i]
+                      : v.data[opt.orderBy[i].field] <= opt.startAt[i]
+                }
+                return ex
+              })(data)
+            }
+          }
+        } else if (!isNil(opt.startAfter)) {
+          const min = Math.min(opt.startAfter.length, opt.orderBy.length)
+          if (min > 0) {
+            for (const i in range(0, min)) {
+              let ex = false
+              data = filter(v => {
+                if (ex === true) return true
+                if (typeof opt.startAfter[i] === "object") {
+                  ex = v.key === opt.startAfter[i].key
+                  if (ex === true) {
+                    return false
+                  }
+                } else {
+                  ex =
+                    opt.orderBy[i].dir === "asc"
+                      ? v.data[opt.orderBy[i].field] > opt.startAfter[i]
+                      : v.data[opt.orderBy[i].field] < opt.startAfter[i]
+                }
+                return ex
+              })(data)
+            }
+          }
+        }
+        if (!isNil(opt.endAt)) {
+          const min = Math.min(opt.endAt.length, opt.orderBy.length)
+          if (min > 0) {
+            for (const i in range(0, min)) {
+              let ex = true
+              data = filter(v => {
+                if (ex === false) return false
+                if (typeof opt.endAt[i] === "object") {
+                  if (v.key === opt.endAt[i].key) {
+                    ex = false
+                    return true
+                  }
+                } else {
+                  ex =
+                    opt.orderBy[i].dir === "asc"
+                      ? v.data[opt.orderBy[i].field] <= opt.endAt[i]
+                      : v.data[opt.orderBy[i].field] >= opt.endAt[i]
+                }
+                return ex
+              })(data)
+            }
+          }
+        } else if (!isNil(opt.endBefore)) {
+          const min = Math.min(opt.endBefore.length, opt.orderBy.length)
+          if (min > 0) {
+            for (const i in range(0, min)) {
+              let ex = true
+              data = filter(v => {
+                if (ex === false) return false
+                if (typeof opt.endBefore[i] === "object") {
+                  if (v.key === opt.endBefore[i].key) ex = false
+                } else {
+                  ex =
+                    opt.orderBy[i].dir === "asc"
+                      ? v.data[opt.orderBy[i].field] < opt.endBefore[i]
+                      : v.data[opt.orderBy[i].field] > opt.endBefore[i]
+                }
+                return ex
+              })(data)
+            }
+          }
+        }
+        if (!isNil(opt.limit)) data = slice(0, opt.limit)(data)
+        res({
+          size: data.length,
+          empty: data.length === 0,
+          forEach: fn => {
+            for (let v of data) {
+              fn({ key: v.key, data: () => v.data })
+            }
+          }
+        })
+      })
+  })
+}
+
+const firestore = () => ({ collection: collection([]) })
+
+firestore.FieldValue = {
+  increment: n => ({
+    key: "increment",
+    val: n,
+    _FieldValue: true
+  }),
+  delete: () => ({
+    key: "delete",
+    _FieldValue: true
+  }),
+  serverTimestamp: () => ({
+    key: "serverTimestamp",
+    _FieldValue: true
+  }),
+  arrayUnion: (...args) => ({
+    val: args,
+    key: "arrayUnion",
+    _FieldValue: true
+  }),
+  arrayRemove: (...args) => ({
+    val: args,
+    key: "arrayRemove",
+    _FieldValue: true
+  })
+}
+
+module.exports = {
+  initializeApp: _config => {
+    config = _config
+  },
+  firestore,
+  db
+}
---
+++ test/test.js
@@ -0,0 +1,263 @@
+const assert = require("assert")
+const firebase = require("../index")
+const fs = firebase.firestore()
+const fv = firebase.firestore.FieldValue
+
+describe("Firestore", function() {
+  describe("collection", function() {
+    it("add", async () => {
+      const col = fs.collection("col")
+      const id = await col.add({ name: "john" })
+      const doc = (await col.doc(id).get()).data()
+      assert.equal(doc.name, "john")
+    })
+    it("get", async () => {
+      const col = fs.collection("col2")
+      const id = await col.add({ name: "john" })
+      const id2 = await col.add({ name: "mary" })
+      const ss = await col.get()
+      const names = ["john", "mary"]
+      let i = 0
+      ss.forEach(doc => assert.equal(doc.data().name, names[i++]))
+      assert.equal(ss.size, 2)
+    })
+    it("limit", async () => {
+      const col = fs.collection("col4")
+      const id = await col.add({ name: "john" })
+      const id2 = await col.add({ name: "mary" })
+      const ss = await col.limit(1).get()
+      assert.equal(ss.size, 1)
+    })
+    it("orderBy", async () => {
+      const col = fs.collection("col5")
+      const id = await col.add({ name: "john", age: 3 })
+      const id2 = await col.add({ name: "mary", age: 3 })
+      const id3 = await col.add({ name: "hide", age: 5 })
+      const ss = await col
+        .orderBy("age", "asc")
+        .orderBy("name", "desc")
+        .get()
+      const names = ["mary", "john", "hide"]
+      let i = 0
+      ss.forEach(doc => assert.equal(doc.data().name, names[i++]))
+      assert.equal(ss.size, 3)
+    })
+    it("where", async () => {
+      const col = fs.collection("col6")
+      const id = await col.add({ name: "john", age: 3 })
+      const id2 = await col.add({ name: "mary" })
+      const id3 = await col.add({ name: "hide", age: 5 })
+      const ss = await col.where("age", "==", 3).get()
+      const names = ["john"]
+      let i = 0
+      ss.forEach(doc => assert.equal(doc.data().name, names[i++]))
+      assert.equal(ss.size, 1)
+    })
+    it("where array", async () => {
+      const col = fs.collection("col7")
+      const id = await col.add({
+        name: "john",
+        age: 3,
+        foods: ["beef", "chicken"]
+      })
+      const id2 = await col.add({ name: "mary", foods: ["pork", "chicken"] })
+      const id3 = await col.add({ name: "hide", age: 5 })
+      const ss = await col.where("name", "in", ["hide"]).get()
+      assert.equal(ss.size, 1)
+      const ss2 = await col.where("foods", "array-contains", "chicken").get()
+      assert.equal(ss2.size, 2)
+      const ss3 = await col.where("name", "not-in", ["john", "hide"]).get()
+      assert.equal(ss3.size, 1)
+      const ss4 = await col
+        .where("foods", "array-contains-any", ["pork", "beef"])
+        .get()
+      assert.equal(ss4.size, 2)
+    })
+    it("startAt", async () => {
+      const col = fs.collection("col8")
+      const id = await col.add({ name: "john", age: 3 })
+      const id2 = await col.add({ name: "mary", age: 3 })
+      const id3 = await col.add({ name: "hide", age: 5 })
+      const ss = await col
+        .orderBy("age", "desc")
+        .orderBy("name", "desc")
+        .startAt(3, "l")
+        .get()
+      const names = ["john"]
+      let i = 0
+      ss.forEach(doc => assert.equal(doc.data().name, names[i++]))
+      assert.equal(ss.size, 1)
+      const doc = await col.doc(id).get()
+      const ss2 = await col
+        .orderBy("age", "desc")
+        .startAt(doc)
+        .get()
+      const names2 = ["john", "mary"]
+      let i2 = 0
+      ss2.forEach(doc => assert.equal(doc.data().name, names2[i2++]))
+      assert.equal(ss2.size, 2)
+    })
+
+    it("startAfter", async () => {
+      const col = fs.collection("col9")
+      const id = await col.add({ name: "john", age: 3 })
+      const id2 = await col.add({ name: "mary", age: 3 })
+      const id3 = await col.add({ name: "hide", age: 5 })
+      const ss = await col
+        .orderBy("age", "desc")
+        .orderBy("name", "desc")
+        .startAfter(4, "mary")
+        .get()
+      const names = ["john"]
+      let i = 0
+      ss.forEach(doc => assert.equal(doc.data().name, names[i++]))
+      assert.equal(ss.size, 1)
+      const doc = await col.doc(id).get()
+      const ss2 = await col
+        .orderBy("age", "desc")
+        .startAfter(doc)
+        .get()
+      const names2 = ["mary"]
+      let i2 = 0
+      ss2.forEach(doc => assert.equal(doc.data().name, names2[i2++]))
+      assert.equal(ss2.size, 1)
+    })
+
+    it("endAt", async () => {
+      const col = fs.collection("col10")
+      const id = await col.add({ name: "john", age: 3 })
+      const id2 = await col.add({ name: "mary", age: 5 })
+      const id3 = await col.add({ name: "hide", age: 5 })
+      const ss = await col
+        .orderBy("age", "desc")
+        .endAt(5)
+        .get()
+      const names = ["mary", "hide"]
+      let i = 0
+      ss.forEach(doc => assert.equal(doc.data().name, names[i++]))
+      assert.equal(ss.size, 2)
+      const doc = await col.doc(id3).get()
+      const ss2 = await col
+        .orderBy("age", "desc")
+        .endAt(doc)
+        .get()
+      const names2 = ["mary", "hide"]
+      let i2 = 0
+      ss2.forEach(doc => assert.equal(doc.data().name, names2[i2++]))
+      assert.equal(ss2.size, 2)
+    })
+
+    it("endBefore", async () => {
+      const col = fs.collection("col11")
+      const id = await col.add({ name: "john", age: 3 })
+      const id2 = await col.add({ name: "mary", age: 3 })
+      const id3 = await col.add({ name: "hide", age: 4 })
+      const ss = await col
+        .orderBy("age", "asc")
+        .endBefore(4)
+        .get()
+      const names = ["john", "mary"]
+      let i = 0
+      ss.forEach(doc => assert.equal(doc.data().name, names[i++]))
+      assert.equal(ss.size, 2)
+      const doc = await col.doc(id2).get()
+      const ss2 = await col
+        .orderBy("age", "desc")
+        .endBefore(doc)
+        .get()
+      const names2 = ["hide", "john"]
+      let i2 = 0
+      ss2.forEach(doc => assert.equal(doc.data().name, names2[i2++]))
+      assert.equal(ss2.size, 2)
+    })
+  })
+  describe("doc", function() {
+    it("get", async () => {
+      const col = fs.collection("doc1")
+      const id = await col.add({ name: "doe" })
+      const data = (await col.doc(id).get()).data()
+      const data2 = (await col.doc("null").get()).data()
+      assert.equal(data.name, "doe")
+      assert.equal(data2, null)
+    })
+    it("set", async () => {
+      const doc = fs.collection("doc2").doc("john")
+      await doc.set({ age: 3, name: "john" })
+      const data = (await doc.get()).data()
+      assert.equal(data.age, 3)
+    })
+    it("update", async () => {
+      const doc = fs.collection("doc3").doc("john")
+      await doc.set({ age: 3, name: "john" })
+      await doc.update({ age: 4 })
+      const data = (await doc.get()).data()
+      assert.equal(data.age, 4)
+    })
+    it("delete", async () => {
+      const doc = fs.collection("doc4").doc("john")
+      await doc.set({ age: 3, name: "john" })
+      await doc.delete()
+      const data = (await doc.get()).data()
+      assert.equal(data, null)
+    })
+  })
+  describe("FieldValue", function() {
+    it("inc", async () => {
+      const col = fs.collection("fv1")
+      const id = await col.add({
+        name: "doe",
+        age: fv.increment(3)
+      })
+      const doc = col.doc(id)
+      await doc.update({ age: fv.increment(-2) })
+      const data = (await col.doc(id).get()).data()
+      assert.equal(data.age, 1)
+    })
+    it("delete", async () => {
+      const col = fs.collection("fv2")
+      const id = await col.add({
+        name: "doe",
+        age: 3
+      })
+      const doc = col.doc(id)
+      await doc.update({ age: fv.delete() })
+      const data = (await col.doc(id).get()).data()
+      assert.equal(data.age, undefined)
+    })
+    it("serverTimestamp", async () => {
+      const col = fs.collection("fv3")
+      const id = await col.add({
+        name: "doe",
+        age: 3,
+        date: fv.serverTimestamp()
+      })
+      const doc = col.doc(id)
+      const data = (await col.doc(id).get()).data()
+      assert.equal(typeof data.date, "number")
+    })
+    it("arrayUnion", async () => {
+      const col = fs.collection("fv4")
+      const id = await col.add({
+        name: "doe",
+        age: 3,
+        foods: fv.arrayUnion("chicken", "beef")
+      })
+      const doc = col.doc(id)
+      await doc.update({ foods: fv.arrayUnion("pork", "lam", "beef") })
+      const data = (await col.doc(id).get()).data()
+      assert.equal(data.foods.length, 4)
+    })
+    it("arrayRemove", async () => {
+      const col = fs.collection("fv5")
+      const id = await col.add({
+        name: "doe",
+        age: 3,
+        foods: fv.arrayUnion("chicken", "beef")
+      })
+      const doc = col.doc(id)
+      await doc.update({ foods: fv.arrayRemove("pork", "lam", "beef") })
+      const data = (await col.doc(id).get()).data()
+      assert.equal(data.foods.length, 1)
+    })
+  })
+})
